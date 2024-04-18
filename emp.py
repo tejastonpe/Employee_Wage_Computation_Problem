f4d123c4b04397f96b61d9438171048b7bb1db5e
[Tejas Tonpe] <[tejastonpe.12345@gmail.com]>
2024-04-18 16:42:38 +0530

    [Tejas Tonpe]Add: Script to claculate daily wage of employee

--- emp.py
+++ emp.py
@@ -1,6 +1,12 @@
 import random
 attendence = random.randint(0,1)
+
+wage_per_hour=20
+daily_hours=8
+
 if attendence==1:
     print("Emplpyee is present")
+    daily_Emp_wage= wage_per_hour * daily_hours
+    print(f"Employees Daily wage is: {daily_Emp_wage}")
 else:
     print("Employee is Absent")
