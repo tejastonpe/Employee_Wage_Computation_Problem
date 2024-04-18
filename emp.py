646b5079a269063f2994822a5ad328da01d2d31d
[Tejas Tonpe] <[tejastonpe.12345@gmail.com]>
2024-04-18 17:43:05 +0530

    [Tejas Tonpe] Add: Used match case to calculate full time & part time employees wage

--- emp.py
+++ emp.py
@@ -5,15 +5,15 @@ wage_per_hour=20
 daily_hours=8
 partTime_hours=4
 
-if attendence==1:
-    print("Emplpyee is present")
-    daily_Emp_wage= wage_per_hour * daily_hours
-    print(f"Employees Daily wage is: {daily_Emp_wage}")
-
-elif attendence==2:
-    print("Part time employee")
-    PartTime_Emp_wage= wage_per_hour * partTime_hours
-    print(f"Part time Employees wage is: {PartTime_Emp_wage}")
-
-else:
-    print("Employee is Absent")
+match attendence:
+    case 1:
+        print("Emplpyee is present")
+        daily_Emp_wage= wage_per_hour * daily_hours
+        print(f"Employees Daily wage is: {daily_Emp_wage}")
+    case 2:
+        print("Part time employee")
+        PartTime_Emp_wage= wage_per_hour * partTime_hours
+        print(f"Part time Employees wage is: {PartTime_Emp_wage}")
+    case 0:
+        print("Employee is Absent")
+        
