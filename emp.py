20a5a221f60e692e8b33369b76f162df4b3537c1
[Tejas Tonpe] <[tejastonpe.12345@gmail.com]>
2024-04-19 14:21:04 +0530

    [Tejas Tonpe] Add: calculated daily wage of employee

--- emp.py
+++ emp.py
@@ -1,22 +1,29 @@
 import random
-attendence = random.randint(0,2)
 
 wage_per_hour=20
 daily_hours=8
-partTime_hours=4
+daily_wage = wage_per_hour * daily_hours
+# part_time_hours=4
+
+def cal_monthly_wages():
+    total_working_days = 0
+    total_wages = 0
+    # attendence=
+
+    while total_working_days <= 20:
+        if  random.randint(0, 1)== 1: 
+            total_working_days += 1
+            total_wages += daily_wage
+
+    return total_wages
 
 
-def fullTime_emp_wage_forMonth(working_day):
-    wage_forMonth= working_day * (wage_per_hour * daily_hours)
-    return wage_forMonth
+monthly_wages = cal_monthly_wages()
+print(f"Monthly wages:{monthly_wages}")
 
-def PartTime_emp_wage_forMonth(working_day):
-    PartTime_emp_wage= working_day * (wage_per_hour * partTime_hours)
-    return PartTime_emp_wage
 
-print(fullTime_emp_wage_forMonth(20))
 
-print(PartTime_emp_wage_forMonth(20))
+
 
 
 
