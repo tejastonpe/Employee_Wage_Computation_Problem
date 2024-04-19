7d66cf2c6408fe4306dcbc67bee566bc7d5dfcea
[Tejas Tonpe] <[tejastonpe.12345@gmail.com]>
2024-04-19 15:24:10 +0530

    [Tejas Tonpe] Add: calculated employees total wage & total working hours using class & object

--- emp.py
+++ emp.py
@@ -1,44 +1,45 @@
 import random
 
-wage_per_hour=20
-daily_hours=8
-partTime_hours=4
+class CalculateWage:
+    def __init__(self, wage_per_hour, daily_hours, part_time_hours, total_hours, total_days):
+        self.wage_per_hour = wage_per_hour
+        self.daily_hours = daily_hours
+        self.part_time_hours = part_time_hours
+        self.total_hours = total_hours
+        self.total_days = total_days
+        self.max_hour = 0
+        self.max_day = 0
 
-total_hours=100
-total_days=20
+    def wage_for_month(self):
+        while (self.max_hour <= self.total_hours or self.max_day <= self.total_days):
+            attendance = random.randint(0, 2)
+            if attendance == 1:
+                self.max_hour += self.daily_hours
+                self.max_day += 1
+                total_wage = self.max_hour * self.max_day * self.wage_per_hour
+                print("Employee is present")
+                print(f"Total Wage:{total_wage} ,Total Hours:{self.max_hour} ")
+           
 
-max_hour = 0
-max_day = 0
+            elif attendance == 2:
+                self.max_hour += self.part_time_hours
+                self.max_day += 1
+                total_wage = self.max_hour * self.max_day * self.wage_per_hour
+                print("Part time Employee")
+                print(f"Total Wage:{total_wage},Total Hours:{self.max_hour} ")
+               
 
-def wage_for_month():
-    global max_hour
-    global max_day
-    while (max_hour <= total_hours or max_day <= total_days):
-        attendence = random.randint(0,2)
-        if attendence==1:
-            max_hour += daily_hours
-            max_day += 1
-            totalWage = max_hour * max_day * wage_per_hour
-            print("Employee is present")
-            print(f"wage:{totalWage}")
-            print(f"Hours:{max_hour}")
-
-        elif attendence==2:
-            max_hour += daily_hours + partTime_hours
-            max_day+= 1
-            totalWage = max_hour * max_day * wage_per_hour
-            print("Part time Employee")
-            print(f"wage:{totalWage}")
-            print(f"Hours:{max_hour}")
-        else:
-            print("Employee is absent")
-
+            else:
+                print("Employee is absent")
         
-    return totalWage
-
-print(wage_for_month())
+        return total_wage
 
 
+wage_per_hour = 20
+daily_hours = 8
+partTime_hours = 4
+total_hours = 100
+total_days = 20
 
-
-
+cal = CalculateWage (wage_per_hour, daily_hours, partTime_hours, total_hours, total_days)
+print(cal.wage_for_month())
