3ba336a0ea2ff364c4be278bb1af6ff980ab21d2
[Tejas Tonpe] <[tejastonpe.12345@gmail.com]>
2024-04-19 10:57:17 +0530

    [Tejas Tonpe] Add: calculated wage till hours reached 100 & 20 days

--- emp.py
+++ emp.py
@@ -1,22 +1,37 @@
 import random
-attendence = random.randint(0,2)
 
 wage_per_hour=20
 daily_hours=8
 partTime_hours=4
 
+total_hours=100
+total_days=20
 
-def fullTime_emp_wage_forMonth(working_day):
-    wage_forMonth= working_day * (wage_per_hour * daily_hours)
-    return wage_forMonth
+max_hour = 0
+max_day = 0
 
-def PartTime_emp_wage_forMonth(working_day):
-    PartTime_emp_wage= working_day * (wage_per_hour * partTime_hours)
-    return PartTime_emp_wage
+def wage_for_month():
+    global max_hour
+    global max_day
+    while (max_hour <= total_hours or max_day <= total_days):
+        attendence = random.randint(0,2)
+        if attendence==1:
+            max_hour += daily_hours
+            max_day += 1
+            totalWage = max_hour * max_day * wage_per_hour
+            print(totalWage)
 
-print(fullTime_emp_wage_forMonth(20))
+        elif attendence==2:
+            max_hour += daily_hours + partTime_hours
+            max_day+= 1
+            totalWage = max_hour * max_day * wage_per_hour
+            print(totalWage)
 
-print(PartTime_emp_wage_forMonth(20))
+        # totalWage = wage_per_hour * daily_hours
+    return totalWage
+
+print(wage_for_month())
+
 
 
 
